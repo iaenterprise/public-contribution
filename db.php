098da00eb803916f84dd89f62d7fbfa407e0989a
iaenterprise <contact@iaenterprise.fr>
2024-05-03 00:55:53 +0200

    Changes

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "inventory-manager";
+$dbname = "yyour-database-name";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
