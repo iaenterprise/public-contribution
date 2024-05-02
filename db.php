f3f5e1ed40afcf55a97f89daa0e74105d581231d
iaenterprise <contact@iaenterprise.fr>
2024-05-03 00:56:45 +0200

    Changes

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "yyour-database-name";
+$dbname = "your-database-name";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
